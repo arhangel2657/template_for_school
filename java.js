3d6b757f0f76471b9114629985f7dbc7d54ac017
arhangel2657 <bebey22881@gmail.com>
2023-11-21 17:13:40 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,18 @@
+//console.error("fsdfs")выводит ошибку
+//var momokm;//переменая
+//momokm = 4;//её число 
+//var nn = 7;
+var ff = 9;
+//var gg = nn + ff;
+//console.warn(nn);выводит переменную
+//console.warn(Math.min(nn , ff));
+//const vfegrb = 8;переменная которую нельзя переделать
+if(ff == 5){
+    console.warn("fgfe sdf ");// !-не равно
+}else if (ff==9){
+    console.warn("foemovefmovmtr");
+}else{
+    console.warn("fefegfegpe")//если значение не равно то выводит это
+}
+
+
